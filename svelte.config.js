eefc81377a1e5fc70421c6a9824f11acce7458d9
happysalada <raphael@megzari.com>
2023-09-17 11:03:31 -0400

    use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,24 @@
-import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-cloudflare';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), preprocess({
-        postcss: true
-    })],
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'edge'
+		})
 	}
 };
 
 export default config;
+

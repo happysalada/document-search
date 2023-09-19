cdfdef6bd76ab86d8fee1b8067c0afab6df124b5
happysalada <raphael@megzari.com>
2023-09-19 06:58:08 -0400

    fix dynamic env var usage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,13 @@
 import type { Actions } from "./$types";
 import { error } from "@sveltejs/kit";
 import { HfInference } from '@huggingface/inference';
-import { UNSTRUCTURED_API_KEY, HUGGINGFACE_API_TOKEN } from "$env/dynamic/public";
+import { env } from "$env/dynamic/private";
 import { getEncoding } from 'js-tiktoken';
 
 // our embedding accept maximum of 512 but we use a different tokenizer
 // so we account for a difference in tokenization
 const CHUNK_SIZE = 500;
-const hf = new HfInference(HUGGINGFACE_API_TOKEN);
+const hf = new HfInference(env.HUGGINGFACE_API_TOKEN);
 
 export const actions = {
   default: async ({ request }) => {
@@ -21,7 +21,7 @@ export const actions = {
           body,
           headers: {
             Accept: "application/json",
-            "unstructured-api-key": UNSTRUCTURED_API_KEY,
+            "unstructured-api-key": env.UNSTRUCTURED_API_KEY,
             "User-Agent": "curl/7.87.0",
           },
         },

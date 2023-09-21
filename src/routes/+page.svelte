ee88825d05e3f800327bd1c96affc3c1d4318eb9
happysalada <raphael@megzari.com>
2023-09-21 06:20:43 -0400

    separate score

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,8 @@
           class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
         >
           <div class="min-w-0 flex-1">
-            <p class="text-sm text-gray-500">{score} {payload.text}</p>
+            <p class="text-sm text-gray-500">score: {score}</p>
+            <p class="text-sm text-gray-500">{payload.text}</p>
           </div>
         </div>
 
